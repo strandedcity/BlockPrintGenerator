26d6760b59fbc0dada5acf9392f800d77026094d
Phil Seaton <phil@phil-seaton.com>
2014-07-04 16:14:46 -0700

    This seems to be doing the right thing, but I can't see it yet

--- js/halftone-processing.js
+++ js/halftone-processing.js
@@ -3,52 +3,26 @@ if (THREE === "undefined") {
 }
 
 function intensitiesForImageWithGridSize(imgEl,gridSize){
-    var blockSize = 5, // only visit every 5 pixels
-        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
-        canvas = document.createElement('canvas'),
+    var canvas = document.createElement('canvas'),
         context = canvas.getContext && canvas.getContext('2d'),
-        data, width, height,
-        i = -4,
-        length,
-        rgb = {r:0,g:0,b:0},
-        count = 0;
-
-    if (!context) {
-        return defaultRGB;
-    }
+        data, width, height;
 
     height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
     width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
 
     context.drawImage(imgEl, 0, 0);
+    data = context.getImageData(0, 0, width, height);
 
-    try {
-        data = context.getImageData(0, 0, width, height);
-    } catch(e) {
-        /* security error, img on diff domain */alert('x');
-        return defaultRGB;
+    var y = 0;
+    while (y*gridSize < height) {
+        var x = 0;
+        while (x*gridSize < width) {
+            var rgb = pixelDensityAtCell(x,y,width,gridSize,data.data);
+            console.log(x,y,rgb);
+            x++;
+        }
+        y++;
     }
-
-    var rgb = pixelDensityAtCell(0,0,width,gridSize,data.data);
-
-    console.log(rgb);
-//    length = data.data.length;
-//
-//
-//
-//    while ( (i += blockSize * 4) < length ) {
-//        ++count;
-//        rgb.r += data.data[i];
-//        rgb.g += data.data[i+1];
-//        rgb.b += data.data[i+2];
-//    }
-//
-//    // ~~ used to floor values
-//    rgb.r = ~~(rgb.r/count);
-//    rgb.g = ~~(rgb.g/count);
-//    rgb.b = ~~(rgb.b/count);
-//
-//    return rgb;
 
 }
 
@@ -64,9 +38,10 @@ function pixelDensityAtCell(x,y,imageWidth,cellsize,data) {
         }
         a++;
     }
-    return {
-        r: ~~(cumulativeR / pixelCount),
-        g: ~~(cumulativeG / pixelCount),
-        b: ~~(cumulativeB / pixelCount)
-    }
+//    return {
+//        r: ~~(cumulativeR / pixelCount),
+//        g: ~~(cumulativeG / pixelCount),
+//        b: ~~(cumulativeB / pixelCount)
+//    }
+    return (cumulativeB + cumulativeG + cumulativeR) / (pixelCount*3*255);
 }
