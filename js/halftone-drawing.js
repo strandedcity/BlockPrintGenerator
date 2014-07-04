a61ebec533e9f973f40e4e7e1a510c83fa99d098
Phil Seaton <phil@phil-seaton.com>
2014-07-04 16:51:45 -0700

    Closer...

--- js/halftone-drawing.js
+++ js/halftone-drawing.js
@@ -9,28 +9,34 @@ function drawHalftoneGridThreeJS(grid) {
     var displayHeight = window.innerHeight;
 
     // renderer
-    var renderer = new THREE.WebGLRenderer();
+    var renderer = new THREE.WebGLRenderer({ antialias: true }   );
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColorHex( 0xffffff, 1 );
     document.body.appendChild(renderer.domElement);
 
     // camera
-    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.z = 1000;
+    var camera = new THREE.OrthographicCamera( displayWidth / - 2, displayWidth / 2, displayHeight / 2, displayHeight / - 2, 1, 1000 );;
+    camera.position.z = 500;
 
     // scene
     var scene = new THREE.Scene();
 
     var blackDotMaterial = new THREE.MeshBasicMaterial({
-        color: 'red'
+        color: 'black'
     });
+
+    var cellSize = 15, maxSize = cellSize / 1.2;
 
     for (var y = 0; y < grid.length; y++) {
         for (var x = 0; x < grid[y].length; x++) {
-            var circle = new THREE.Mesh(new THREE.CircleGeometry(grid[y][x]*20,20),blackDotMaterial);
-            circle.position = new THREE.Vector3( x*20-displayWidth/2, y*20-displayHeight/2, 0);
+            var circle = new THREE.Mesh(new THREE.CircleGeometry(grid[y][x]*maxSize,15),blackDotMaterial);
+            circle.position = new THREE.Vector3( x*cellSize, y*cellSize, 0);
             scene.add(circle);
         }
     }
+
+    camera.position.x = cellSize * grid[0].length / 2;
+    camera.position.y = cellSize * grid.length / 2;
 
     renderer.render(scene,camera);
 }
